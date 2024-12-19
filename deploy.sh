36fade5c8ab1997580d2af53f5155666819d8c41
jayson <jiwanaga@hotmail.com>
2024-12-18 14:18:32 -1000

    update

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,3 @@ TRAEFIK_COMPOSE_DIR="$HOME/docker/traefik_compose"
 
 cd $TRAEFIK_COMPOSE_DIR
 chmod +x deploy_traefik_compose.sh
-
-###################################################################################
-## Clone the Github repository
-###################################################################################
-# Clone traefik_compose repository (if not already present)
-if [ ! -d $TRAEFIK_COMPOSE_DIR ]; then
-  echo -e "${green}The git repolistory $TRAEFIK_COMPOSE_DIR not found. Cloning the repository...${reset}"
-  git clone --quiet -b main https://github.com/guanz808/traefik_compose.git $TRAEFIK_COMPOSE_DIR
-else
-  echo -e "${green}The git repository $TRAEFIK_COMPOSE_DIR already exists. Updating...${reset}"
-  git -C $TRAEFIK_COMPOSE_DIR pull origin main #--quiet 
-fi
-
-# Run deploy_traefik_compose.sh
-./deploy_traefik_compose.sh
